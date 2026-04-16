c3e91db5386068f211de40e9b909d52c14376c61
Rashedul2001 <hacker360sr@gmail.com>
2026-04-16 19:17:47 +0600

    Implement mobile navigation bar and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,28 +8,41 @@ import { IoCloseSharp } from "react-icons/io5";
 import { useState } from 'react';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [open, setOpen] = useState(true);
 
     return (
-        <nav className='relative flex justify-between items-center px-10 h-20 '>
+        <nav className='flex justify-between items-center px-10 h-10 '>
             <div className=''>
                 <NavLink to="/"  ><img src={logo} alt="Logo Keen Keeper" className='' /></NavLink>
             </div>
-            <div className='sm:hidden z-10' onClick={() => setIsOpen(!isOpen)}>
-                {
-                    isOpen ? <IoCloseSharp /> : <FaBars />
-                }
+
+
+            <div className="drawer drawer-end sm:hidden ">
+                <input id="my-drawer-5" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex justify-end">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer-5" className="drawer-button z-20 cursor-pointer" onClick={() => setOpen(!open)} >{open ? <FaBars /> : <IoCloseSharp />}</label>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-5" aria-label="close sidebar" className="drawer-overlay" onClick={() => setOpen(!open)}></label>
+                    <div className="menu bg-base-200 w-2/3 min-h-full max-w-40 p-4 rounded-tl-lg">
+                        <div className='mt-10'>
+
+                            <NavLink to="/" className={({ isActive }) => ` flex items-center gap-1 p-2 rounded-sm font-medium ${isActive ? "bg-green-900 text-white" : "hover:bg-green-100"}`}><RiHome2Line /> Home</NavLink>
+                            <NavLink to="/timeline" className={({ isActive }) => `flex items-center gap-1 p-2 rounded-sm font-medium ${isActive ? "bg-green-900 text-white" : "hover:bg-green-100"}`}><CiClock2 /> Timeline</NavLink>
+                            <NavLink to="/stats" className={({ isActive }) => `flex items-center gap-1 p-2 rounded-sm font-medium ${isActive ? "bg-green-900 text-white" : "hover:bg-green-100"}`}><AiOutlineStock /> Stats</NavLink>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
             <div className='hidden sm:flex items-center gap-4 '>
                 <NavLink to="/" className={({ isActive }) => `flex items-center gap-1 p-2 rounded-sm font-medium ${isActive && "bg-green-900 text-white"}`}><RiHome2Line /> Home</NavLink>
                 <NavLink to="/timeline" className={({ isActive }) => `flex items-center gap-1 p-2 rounded-sm font-medium ${isActive ? "bg-green-900 text-white" : ""}`}><CiClock2 /> Timeline</NavLink>
                 <NavLink to="/stats" className={({ isActive }) => `flex items-center gap-1 p-2 rounded-sm font-medium ${isActive ? "bg-green-900 text-white" : ""}`}><AiOutlineStock /> Stats</NavLink>
             </div>
-            {/* <div className={`absolute sm:hidden ${isOpen ? " right-12" : "-right-40"} top-10 space-y-1 bg-gray-100 rounded-sm transition-all duration-300`} onClick={() => setIsOpen(!isOpen)} >
-                <NavLink to="/" className={({ isActive }) => `flex items-center gap-1 p-1 rounded-sm font-medium ${isActive && "bg-green-900 text-white"}`}><RiHome2Line /> Home</NavLink>
-                <NavLink to="/timeline" className={({ isActive }) => `flex items-center gap-1 p-1 rounded-sm font-medium ${isActive ? "bg-green-900 text-white" : ""}`}><CiClock2 /> Timeline</NavLink>
-                <NavLink to="/stats" className={({ isActive }) => `flex items-center gap-1 p-1 rounded-sm font-medium ${isActive ? "bg-green-900 text-white" : ""}`}><AiOutlineStock /> Stats</NavLink>
-            </div> */}
+
         </nav>
     );
 };
